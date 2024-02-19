fdd97b29414d5bb92b8bf45828ec03794cbf841b
GersonArlindo <gerson50039@gmail.com>
2024-02-19 14:50:52 -0600

    Adicionando parametros

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.get('/initiate/:clientId/:clientSecret', require('./lib/initiate'));
 
 //app.get('/refresh', require('./lib/refresh'));
 
-app.get('/oauth/callback/:clientId/:clientSecret', require('./lib/callback'));
+app.get('/oauth/callback', require('./lib/callback'));
 
 app.get('/', require('./lib/welcome'));
 
