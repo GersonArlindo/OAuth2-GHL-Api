eaec2d15153b38d402dbe6864ed7802a10934a23
GersonArlindo <gerson50039@gmail.com>
2024-02-20 10:29:42 -0600

    Adicionando parametros

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ app.get('/oauth/callback', require('./lib/callback'));
 
 app.get('/', require('./lib/welcome'));
 
+app.set('trust proxy', true);
+
 app.listen(3000, () => {
     console.log("App Listening on 3000 !");
 });
